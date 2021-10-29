5078914e0aff3b3c0c773634bfe12407f3649709
SudhamaniRM <sudhamani.rm@khoros.com>
2021-10-29 11:37:04 +0530

    This is class returns message which should be posted

---
+++ src/main/java/com/Twitter_Project/Request.java
@@ -0,0 +1,11 @@
+package com.Twitter_Project;
+
+public class Request {
+    String msg;
+    public String getMsg(){
+        return msg;
+    }
+    public void setMsg(String msg){
+        this.msg=msg;
+    }
+}
