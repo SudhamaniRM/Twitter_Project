5a80c0db5940b9826649932723d9adce8e32a524
SudhamaniRM <sudhamani.rm@khoros.com>
2021-11-18 10:32:03 +0530

    Updated Request class

--- src/main/java/com/Twitter_Project/resources/Request.java
+++ src/main/java/com/Twitter_Project/resources/Request.java
@@ -1,2 +1,20 @@
-package com.Twitter_Project.resources;public class Request {
+package com.Twitter_Project.resources;
+
+public class Request {
+    String msg;
+
+    public Request(String msg) {
+        this.msg = msg;
+    }
+
+    public Request() {
+    }
+
+    public String getMsg() {
+        return msg;
+    }
+
+    public void setMsg(String msg) {
+        this.msg = msg;
+    }
 }
